7d81108f92abdf1fccc8fbf204b87b5d7d6e64cd
Vasco Santos <vasco.santos@moxy.studio>
2018-08-28 16:37:10 +0100

    chore: remove unused instance property

--- src/core/index.js
+++ src/core/index.js
@@ -86,7 +86,6 @@ class IPFS extends EventEmitter {
     this._bitswap = undefined
     this._blockService = new BlockService(this._repo)
     this._ipld = new Ipld(this._blockService)
-    this._pubsub = undefined
     this._preload = preload(this)
     this._mfsPreload = mfsPreload(this)
 
